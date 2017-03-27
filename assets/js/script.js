71b5b486693cc66f4b413390fa1bf400200b6502
Cara Chew <carachew@gmail.com>
2017-03-27 16:39:32 +0800

    Completed Fns up to returning if the guess letter is in the word

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,13 +5,14 @@ $(document).ready(function() {
   var puzzle =
     [
     {answer: "BUN", category: "FOOD"} ,
-    {answer: "CAKE", category:"FOOD"},
-    {answer: "PUFF", category:"FOOD"},
+    {answer: "ELDERFLOWER", category:"FLOWER"},
+    {answer: "GREENLAND", category:"COUNTRY"},
     ]
 
   // var answer = ['PIZZA', 'PASSIONFRUITS', 'CHEESEBURGERS', 'BLACKBERRIES', 'GINGERBREAD', 'POMEGRANATE', 'QUESADILLAS', 'LEMONGRASS', 'PISTACHIOS', 'WATERMELON']
 
   var puzzleAlreadyDisplayed = []
+  var listedWords = []
 
   var pOneName = ''
   var pTwoName = ''
@@ -42,19 +43,107 @@ $(document).ready(function() {
 
     var display = document.querySelector('#display-puzzle')
 
-    console.log("the answer length is : " + answer.length)
+    console.log("the answer is: " + answer + " and the answer length is : " + answer.length)
 
-    for(var i = 0; i = answer.length; i++) {
+    for(var i = 0; i < answer.length; i++) {
+
+      // Separate as Letter of the Answer into 1 Box
       var letterOfWordBox = document.createElement('div')
-      letterOfWordBox.id = 'letter'
+      letterOfWordBox.className = 'letter'
+      letterOfWordBox.textContent = answer.split('')[i]
       display.appendChild(letterOfWordBox)
     }
 
+    // Display corresponding Category below the boxes
+    var categoryIntro = document.createElement('div')
+    categoryIntro.textContent = "The category of this puzzle is:"
+    display.appendChild(categoryIntro)
+
+    var showCategory = document.createElement('div')
+    showCategory.id = 'category'
+    showCategory.textContent = category
+    display.appendChild(showCategory)
 
     console.log(answer, category)
   }
 
-  // displayPuzzle()
+  displayPuzzle()
+
+  function keyInMethod() {
+    var inputSpot = document.querySelector('#inputSpotForGuess')
+
+    var newLabel = document.createElement('label')
+    newLabel.setAttribute("for","guess")
+
+    var input = document.createElement('input')
+    input.setAttribute('type','text')
+    input.id = 'guess'
+
+    var button = document.createElement('input')
+    button.setAttribute('type','button')
+    button.setAttribute('value','Submit')
+    button.id = 'guessButton'
+
+    inputSpot.appendChild(newLabel)
+    inputSpot.appendChild(input)
+    inputSpot.appendChild(button)
+
+  }
+
+  keyInMethod()
+
+
+  function checkGuess() {
+    var input = document.querySelector('#guess')
+    var button = document.querySelector('#guessButton')
+    var answer = puzzle[randomIndex].answer
+
+      if (answer.includes(input.value.toUpperCase())) {
+        // console.log (input.value + " is in the " + answer)
+        return true
+      } else {
+        // console.log (input.value + " is not in the " + answer)
+        return false
+      }
+  }
+
+  // checkGuess()
+
+  function guessResult() {
+    var input = document.querySelector('#guess')
+    var showResultBox = document.querySelector('.interaction')
+    var showResult = document.createElement('p')
+    showResult.id = 'showResult'
+
+    showResultBox.removeChild(showResultBox.lastChild)
+
+    if (checkGuess() === true) {
+      showResult.textContent = "Great Job! " + input.value.toUpperCase() + " is in the word."
+      showResultBox.appendChild(showResult)
+
+    } else {
+      showResult.textContent = "Sorry, " + input.value.toUpperCase() + " is not in the word."
+      showResultBox.appendChild(showResult)
+
+    }
+  }
+
+  // guessResult()
+
+  function iWantToSolve() {
+
+  }
+
+  var button = document.querySelector('#guessButton')
+
+  button.addEventListener('click', function () {
+    checkGuess()
+    guessResult()
+  })
+
+  // document.addEventListener('click', function() {
+  //   clearResult()
+  // })
 
   // function puzzleDisplayed() {
   //   if (puzzleAlreadyDisplayed.includes(randomIndex) === true) {
